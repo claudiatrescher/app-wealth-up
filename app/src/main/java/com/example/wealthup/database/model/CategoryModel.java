c7488a531e1ab22f5556226ef57a98d415a5deb3
Daniel Mafioleti <danielmafioleti2@gmail.com>
2025-06-26 19:04:20 -0300

    models and daos

--- app/src/main/java/com/example/wealthup/database/model/CategoryModel.java
+++ app/src/main/java/com/example/wealthup/database/model/CategoryModel.java
@@ -1,19 +1,27 @@
 package com.example.wealthup.database.model;
 
-public class Category {
+public class CategoryModel {
+
+    public static final String TABLE_NAME = "tb_category";
+    public static final String
+            COLUNA_ID = "_id",
+            COLUNA_NAME = "name",
+            COLUNA_COLOR = "color";
+
+    //  Script de criação da tabela
+    public static final String
+            CREATE_TABLE = "CREATE TABLE " + TABLE_NAME
+            + " ( "
+            + COLUNA_ID + " integer primary key autoincrement, "
+            + COLUNA_NAME + " text not null, "
+            + COLUNA_COLOR + " text not null, "
+            + " );";
+
+    public static final String
+            DROP_TABLE =  "drop table if exist " + TABLE_NAME + ";";
     private int id;
     private String name;
     private String color;
-
-    public Category() {
-
-    }
-
-    public Category(int id, String name, String color) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-    }
 
     // Getters
     public int getId() {
